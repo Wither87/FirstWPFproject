1faa343df7182a3a12d85f26b10ca9a2050a7831
Wither_87 <wither_87@mail.ru>
2020-03-19 18:23:20 +0500

    ComboBox

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -9,7 +11,6 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
-
         public MainWindow()
         {
             InitializeComponent();
@@ -22,26 +23,147 @@ namespace WpfApplication1
 
         private void PlusButton_Click(object sender, RoutedEventArgs e)
         {
-            CreateTextBox();
+            CreateDockPanel();
         }
 
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
-            DeleteTextBox();
-            Sum();
+            DeleteDockPanel();
         }
 
+        /// <summary>
+        /// Горячие клавиши для управления программой.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Window_KeyUp(object sender, KeyEventArgs e)
         {
             switch (e.Key)
             {
-                case Key.Space: CreateTextBox(); break;
-                case Key.Escape: Close(); break;
-                default: Sum(); break;
+                case Key.Space: CreateTextBox(); break; // Создать новый TextBox
+                case Key.Escape: Close(); break;        // Закрыть приложение
+                default: Calculate(); break;            // Посчитать
             }
         }
 
-        void CreateTextBox()
+        /// <summary>
+        /// Создаёт Докпанель
+        /// </summary>
+        void CreateDockPanel()
+        {
+            DockPanel dock = new DockPanel { Margin = new Thickness(5) };   // Создать докпанель с отступом в 5 единиц
+            dock.Children.Add(CreateTextBox());                             // Добавление TextBox в докпанель
+            dock.Children.Add(CreateComboBox());                            // Добавление комбобокса в докпанель
+            StackPanelWithTextBoxes.Children.Add(dock);                     // Добавление докпанели в Стекпанель на форме
+        }
+
+        /// <summary>
+        /// Считает содержимое всех Текстбоксов
+        /// </summary>
+        void Calculate()
+        {
+            foreach (var item in StackPanelWithTextBoxes.Children) 
+            {
+                if (item is DockPanel)
+                {
+                    DockPanel dock = (DockPanel)item;
+                    TextBox textInTextBox = LookTextBox(dock);  // Найти TextBox
+                    ComboBox comb = LookComboBox(dock);         // Найти ComboBox
+                    int num;
+                    try { num = int.Parse(textInTextBox.Text); }// Попробовать пропарсить содержимое TextBox
+                    catch {
+                        if (textInTextBox.Text == "") { }       // ничего не делать если TextBox пустой
+                        else { MessageBox.Show("Данные введены не корректно"); textInTextBox.Text = ""; }   // Выдать MessageBox с ошибкой, очищает TextBox
+                        continue;
+                    }
+                    switch (comb.SelectedItem) // Определение выделеного элемента в ComboBox
+                    {
+                        case "+":
+                            result += num;
+                            break;
+                        case "-":
+                            result -= num;
+                            break;
+                        case "*":
+                            result *= num;
+                            break;
+                        case "/":
+                            result /= num;
+                            break;
+                    }                     
+                }
+            }
+            label.Content = startText + result;  // Вывод результата
+            result = 0;
+        }
+
+        /// <summary>
+        /// Удаляет последнюю DockPanel из Стекпанели
+        /// </summary>
+        void DeleteDockPanel()
+        {
+            int stackPanelLength = StackPanelWithTextBoxes.Children.Count - 1;  // Последний индекс на 1 меньше длинны коллекции
+            for (int i = stackPanelLength; i >= 0; i--)
+                if (StackPanelWithTextBoxes.Children[i] is DockPanel)
+                {
+                    StackPanelWithTextBoxes.Children.RemoveAt(i); // Удалить DockPanel
+                    break;
+                }
+            Calculate();
+        }
+
+        /// <summary>
+        /// Ищет ComboBox в докпанели
+        /// </summary>
+        /// <param name="dock"></param>
+        /// <returns></returns>
+        ComboBox LookComboBox(DockPanel dock)
+        {
+            foreach (var item in dock.Children)
+                if (item is ComboBox)
+                    return item as ComboBox;
+            return null;
+        }
+
+        /// <summary>
+        /// Ищет TextBox в докпанели
+        /// </summary>
+        /// <param name="dock"></param>
+        /// <returns></returns>
+        TextBox LookTextBox(DockPanel dock)
+        {
+            foreach (var item in dock.Children)
+                if (item is TextBox)
+                    return item as TextBox;
+            return null;
+        }
+
+        /// <summary>
+        /// Создаёт ComboBox
+        /// </summary>
+        /// <returns></returns>
+        ComboBox CreateComboBox()
+        {
+            ComboBox comb = new ComboBox
+            {
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Width = 50,
+            };
+            comb.Items.Add("+");
+            comb.Items.Add("-");
+            comb.Items.Add("*");
+            comb.Items.Add("/");
+            comb.SelectedItem = "+";
+            comb.SelectionChanged += Comb_SelectionChanged;
+            return comb;
+        }
+
+        /// <summary>
+        /// Создаёт TextBox
+        /// </summary>
+        /// <returns></returns>
+        TextBox CreateTextBox()
         {
             txt = new TextBox
             {
@@ -50,55 +172,18 @@ namespace WpfApplication1
                 Height = 20,
                 Width = 100,
             };
-            txt.TextChanged += Txt_TextChanged;
-            StackPanelWithTextBoxes.Children.Add(txt);
+            txt.TextChanged += Txt_Sum;
+            return txt;
         }
 
-        void DeleteTextBox()
+        private void Comb_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            int stackPanelLength = StackPanelWithTextBoxes.Children.Count-1;
-            for (int i = stackPanelLength; i >= 0; i--)
-            {
-                if (StackPanelWithTextBoxes.Children[i] is TextBox)
-                {
-                    StackPanelWithTextBoxes.Children.RemoveAt(i);
-                    break;
-                }
-            }
+            Calculate();
         }
 
-        private void Txt_TextChanged(object sender, TextChangedEventArgs e)
+        private void Txt_Sum(object sender, TextChangedEventArgs e)
         {
-            Sum();
-        }
-
-        void Sum()
-        {
-            result = 0;
-            foreach (var item in StackPanelWithTextBoxes.Children)
-            {
-                if (item is TextBox)
-                {
-                    TextBox textInTextBox = (TextBox)item;
-                    try
-                    {
-                        int num = int.Parse(textInTextBox.Text);
-                        result += num;
-                    }
-                    catch
-                    {
-                        if (textInTextBox.Text == "") { }
-                        else
-                        {
-                            MessageBox.Show("Данные введены не корректно");
-                            textInTextBox.Text.Remove(textInTextBox.Text.Length - 1);
-                        }
-                        continue;
-                    }
-                }
-            }
-            label.Content = startText + result;
-            result = 0;
+            Calculate();
         }
     }
 }
